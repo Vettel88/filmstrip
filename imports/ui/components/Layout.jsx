c9329268318b04cada73037b8564dea3f10a4902
Dave Rogers <dave@incnition.com>
2019-08-08 14:40:33 -0700

    changing the color of the nav bar

--- imports/ui/components/Layout.jsx
+++ imports/ui/components/Layout.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom"
-import { Grid as UnstyledGrid, Drawer, List, ListItem as LI, DrawerHeader, DrawerContent, DrawerSubtitle, DrawerTitle, GridCell, GridInner, TopAppBar, TopAppBarSection, TopAppBarNavigationIcon, TopAppBarRow, TopAppBarTitle } from 'rmwc'
+import { Grid as UnstyledGrid, Drawer, List, ListItem as LI, DrawerHeader, DrawerContent, DrawerSubtitle, DrawerTitle, GridCell, GridInner, TopAppBar as UnstyledTopAppBar, TopAppBarSection, TopAppBarNavigationIcon, TopAppBarRow, TopAppBarTitle } from 'rmwc'
 import styled from 'styled-components'
 
 const Grid = styled(UnstyledGrid)`
   padding-top: 100px !important;
+`
+
+const TopAppBar = styled(UnstyledTopAppBar)`
+  background-color: #25455b !important;
 `
 
 const Header = () => {
