0f26e2cc3db20155985a7e1cc69c1fc4aedf2d3f
Dave Rogers <dave@incnition.com>
2019-08-08 12:25:36 -0700

    fixing the drawer and top bar

--- imports/ui/components/Layout.jsx
+++ imports/ui/components/Layout.jsx
@@ -1,30 +1,39 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom"
-import { Grid, Drawer, List, ListItem, DrawerHeader, DrawerContent, DrawerSubtitle, DrawerTitle, GridCell, GridInner, TopAppBar, TopAppBarSection, TopAppBarNavigationIcon, TopAppBarRow, TopAppBarTitle } from 'rmwc'
+import { Grid as UnstyledGrid, Drawer, List, ListItem, DrawerHeader, DrawerContent, DrawerSubtitle, DrawerTitle, GridCell, GridInner, TopAppBar, TopAppBarSection, TopAppBarNavigationIcon, TopAppBarRow, TopAppBarTitle } from 'rmwc'
+import styled from 'styled-components'
 
-const Header = () => (
-    <TopAppBar>
-        <TopAppBarRow>
-            <TopAppBarSection>
-                <SideNav />
-                <TopAppBarTitle>
-                    {UIState.name}
-                </TopAppBarTitle>
-            </TopAppBarSection>
-            <TopAppBarSection>
-                <div id="userInfo">
-                    Userinfo
-                </div>
-            </TopAppBarSection>
-        </TopAppBarRow>
-    </TopAppBar>
-)
+const Grid = styled(UnstyledGrid)`
+  padding-top: 100px !important;
+`
 
-const SideNav = () => {
+const Header = () => {
   const [open, setOpen] = React.useState(false);
   return (
+    <>
+        <TopAppBar fixed={false}>
+            <TopAppBarRow>
+                <TopAppBarNavigationIcon icon="menu" onClick={() => setOpen(true)} />
+                <TopAppBarSection>
+                    <TopAppBarTitle>
+                        {UIState.name}
+                    </TopAppBarTitle>
+                </TopAppBarSection>
+                <TopAppBarSection>
+                    <div id="userInfo">
+                        Userinfo
+                    </div>
+                </TopAppBarSection>
+            </TopAppBarRow>
+        </TopAppBar>
+        <SideNav open={open} setOpen={setOpen} />
+    </>
+  )
+}
+
+const SideNav = ({open, setOpen}) => {
+  return (
       <>
-          <TopAppBarNavigationIcon icon="menu" onClick={() => setOpen(true)} />
           <Drawer modal open={open} onClose={() => setOpen(false)}>
               <DrawerHeader>
                   <DrawerTitle>DrawerHeader</DrawerTitle>
@@ -48,11 +57,9 @@ export default class Layout extends Component {
             <>
                 <Header />
                 <Grid>
-                    <GridInner>
-                        <GridCell>
-                            {this.props.children}
-                        </GridCell>
-                    </GridInner>
+                    <GridCell>
+                        {this.props.children}
+                    </GridCell>
                 </Grid>
             </>
         )
