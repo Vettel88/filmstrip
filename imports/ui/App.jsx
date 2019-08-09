9757861c3279d548853e1d8946629424255666e8
Chuck Carpenter <chuck.carpenter@me.com>
2019-08-09 16:14:01 -0700

    feat: add filmstrip component and organize for upload

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
-import { Button } from '@rmwc/button'
 import { List, ListItem } from '@rmwc/list'
-import ReactFilestack from 'filestack-react'
 import Layout from './components/Layout'
 
 import UIState from './UIState.js'
+import { Filmstrip } from './components/Filmstrip.jsx'
+import { Hello } from './components/Hello.jsx'
 import { QueuesList } from './components/QueuesList.jsx'
 import { QueueItem } from './components/QueueItem.jsx'
 import { SignUp } from '/imports/ui/components/users/SignUp.jsx'
@@ -31,6 +31,7 @@ export default App = () =>
             <AppRoute exact path="/" component={Home} layout={Layout} />
             <AppRoute exact path="/queueList" component={QueuesList} layout={Layout} />
             <AppRoute exact path="/frameList" component={FrameList} layout={Layout} />
+            <AppRoute exact path="/filmstrip" component={Filmstrip} layout={Layout} />
             <AppRoute exact path="/frame" component={Frame} layout={Layout} />
             <AppRoute exact path="/signUp" component={SignUp} layout={Layout} />
             <AppRoute exact path="/signIn" component={SignIn} layout={Layout} />
@@ -41,31 +42,5 @@ export default App = () =>
     </Router>
 
 const Home = () => <h2>Home</h2>
-const Frame = ({ match }) => 
-    <>
-        <h3>Requested Param: {match.params.id}</h3>
-        <ReactFilestack
-            apikey={'Aqp34KkGdTvCthMIbNKTYz'}
-            onSuccess={(res) => console.log(match)}
-            render={({ onPick }) => (
-                <Button label='+ Upload File' onClick={onPick} />
-            )}
-        />
-    </>
-const FrameList = ({ match }) =>
-    <>
-        <h2>FrameList</h2>
-        <List>
-            <ListItem><Link to={`${match.url}/frame1`}>Frame1</Link></ListItem>
-            <ListItem><Link to={`${match.url}/frame2`}>Frame2</Link></ListItem>
-        </List>
-
-        <Route path={`${match.path}/:id`} component={Frame} />
-        <Route
-            exact
-            path={match.path}
-            render={() => <h3>Please select a frame.</h3>}
-        />
-    </>
 
 const NoMatch = (props) => <div>404 - not your day</div>
