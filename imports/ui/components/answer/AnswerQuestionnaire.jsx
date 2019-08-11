7fb5d38ba69bc270beeeb41926d2950184296081
Juho Hietala <hietala@gmail.com>
2019-08-10 20:13:13 -0700

    frame POJO

--- imports/ui/components/answer/AnswerQuestionnaire.jsx
+++ imports/ui/components/answer/AnswerQuestionnaire.jsx
@@ -6,7 +6,7 @@ import { Queues } from '/imports/db/queues.js'
 import { loadingWrapper, emailIsValid } from '/imports/ui/UIHelpers.js'
 import { TextField, Button, Typography } from 'rmwc'
 import { withTranslation } from 'react-i18next'
-import { AnswerQuestion } from './AnswerQuestion.jsx';
+import { AnswerQuestion } from './AnswerQuestion.jsx'
 
 class AnswerQuestionnaireContainer extends React.Component {
 
@@ -54,6 +54,49 @@ export const AnswerQuestionnaire = withTranslation()(withTracker(({ match }) => 
     return {
         isLoading: !handle.ready(),
         queueItem: Queues.findOne(),
-        email: atob(match.params.emailBase64)
+        email: atob(match.params.emailBase64),
+        frames: [
+            {
+                "_id": "1",
+                "name": "Filmstrip 1",
+                "description": "Description of Filmstrip 1",
+                "frames": [
+                {
+                    "no": 1,
+                    "title": "Frame1",
+                    "description": "Description 1",
+                    "link": "https://filmstrip.com",
+                    "files": [
+                    {
+                        "filename": "a1498d35-5092-4c85-9895-a52aab24eddc.jpeg",
+                        "handle": "SU8Lk6RlQXqa7zya6NN6",
+                        "mimetype": "image/jpeg",
+                        "originalPath": "a1498d35-5092-4c85-9895-a52aab24eddc.jpeg",
+                        "size": 276444,
+                        "source": "local_file_system",
+                        "url": "https://cdn.filestackcontent.com/SU8Lk6RlQXqa7zya6NN6",
+                        "uploadId": "rbjaTqbGFRh7UpEY",
+                        "originalFile": {
+                        "name": "a1498d35-5092-4c85-9895-a52aab24eddc.jpeg",
+                        "type": "image/jpeg",
+                        "size": 276444
+                        },
+                        "status": "Stored"
+                    }
+                    ]
+                },
+                {
+                    "no": 2,
+                    "title": "Frame2",
+                    "description": "Description 2",
+                    "link": "https://filmstrip.com/2"
+                }
+                ],
+                "createdAt": "2019-08-10T01:03:47.073Z",
+                "createdBy": "system",
+                "modifiedAt": "2019-08-10T01:04:51.582Z",
+                "modifiedBy": "system"
+            }
+        ]
     }
 })(AnswerWrapper))
