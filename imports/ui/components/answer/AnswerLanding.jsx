a674b4eff5126dba57d45e4896828777724c9415
Juho Hietala <hietala@gmail.com>
2019-08-12 09:26:09 -0700

    merge + from queues to filmstrips

--- imports/ui/components/answer/AnswerLanding.jsx
+++ imports/ui/components/answer/AnswerLanding.jsx
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor'
 import React from 'react'
 import { Route, Link, Redirect } from 'react-router-dom'
 import { withTracker } from 'meteor/react-meteor-data'
-import { Queues } from '/imports/db/queues.js'
+import { Filmstrips } from '/imports/db/filmstrips.js'
 import { loadingWrapper, emailIsValid } from '/imports/ui/UIHelpers.js'
 import { TextField, Button, Typography } from 'rmwc'
 import { withTranslation } from 'react-i18next'
@@ -45,7 +45,7 @@ class AnswerHome extends React.Component {
         return (
             <div className='centered AnswerLanding'>
                 <img src='/icons8-short_hair_girl_question_mark.svg' className='topIcon centered' />
-                <h4><Typography use='headline4'>{this.props.item.title}</Typography></h4>
+                <h4><Typography use='headline4'>{this.props.item.name}</Typography></h4>
                 <p><Typography use='body1'>{this.props.item.description}</Typography></p>
                 <h6><Typography use='body2'>{t('AnswerLandingHelp')}</Typography></h6>
                 <form onSubmit={this.handleSubmit}>
@@ -85,10 +85,10 @@ const AnswerWrapper = ({ isLoading, queueItem, email, t }) => {
 
 export const AnswerLanding = withTranslation()(withTracker(({ match }) => {
     const id = match.params.id
-    const handle = Meteor.subscribe('Queue', id)
+    const handle = Meteor.subscribe('Filmstrip', id)
     return {
         isLoading: !handle.ready(),
-        queueItem: Queues.findOne(),
+        queueItem: Filmstrips.findOne(),
         email: match.params.emailBase64 ? atob(match.params.emailBase64) : ''
     }
 })(AnswerWrapper))
--- imports/ui/components/answer/AnswerQuestionnaire.jsx
+++ imports/ui/components/answer/AnswerQuestionnaire.jsx
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor'
 import React from 'react'
 import { Route, Link, Redirect } from 'react-router-dom'
 import { withTracker } from 'meteor/react-meteor-data'
-import { Queues } from '/imports/db/queues.js'
+import { Filmstrips } from '/imports/db/filmstrips.js'
 import { loadingWrapper, emailIsValid } from '/imports/ui/UIHelpers.js'
 import { TextField, Button, Typography } from 'rmwc'
 import { withTranslation } from 'react-i18next'
@@ -33,7 +33,7 @@ class AnswerQuestionnaireContainer extends React.Component {
 
         return (
             <div className='centered AnswerLanding'>
-                <h5><Typography use='headline5'>{this.props.item.title}</Typography></h5>
+                <h5><Typography use='headline5'>{this.props.item.name}</Typography></h5>
                 <AnswerQuestion item={this.props.item} frame={this.state.currentFrameNumber} />
             </div>
         )
@@ -50,10 +50,10 @@ const AnswerWrapper = ({ isLoading, queueItem, email, t }) =>
 
 export const AnswerQuestionnaire = withTranslation()(withTracker(({ match }) => {
     const id = match.params.id
-    const handle = Meteor.subscribe('Queue', id)
+    const handle = Meteor.subscribe('Filmstrip', id)
     return {
         isLoading: !handle.ready(),
-        queueItem: Queues.findOne(),
+        queueItem: Filmstrips.findOne(),
         email: atob(match.params.emailBase64),
         frames: [
             {
@@ -61,36 +61,36 @@ export const AnswerQuestionnaire = withTranslation()(withTracker(({ match }) => 
                 "name": "Filmstrip 1",
                 "description": "Description of Filmstrip 1",
                 "frames": [
-                {
-                    "no": 1,
-                    "title": "Frame1",
-                    "description": "Description 1",
-                    "link": "https://filmstrip.com",
-                    "files": [
                     {
-                        "filename": "a1498d35-5092-4c85-9895-a52aab24eddc.jpeg",
-                        "handle": "SU8Lk6RlQXqa7zya6NN6",
-                        "mimetype": "image/jpeg",
-                        "originalPath": "a1498d35-5092-4c85-9895-a52aab24eddc.jpeg",
-                        "size": 276444,
-                        "source": "local_file_system",
-                        "url": "https://cdn.filestackcontent.com/SU8Lk6RlQXqa7zya6NN6",
-                        "uploadId": "rbjaTqbGFRh7UpEY",
-                        "originalFile": {
-                        "name": "a1498d35-5092-4c85-9895-a52aab24eddc.jpeg",
-                        "type": "image/jpeg",
-                        "size": 276444
-                        },
-                        "status": "Stored"
+                        "no": 1,
+                        "title": "Frame1",
+                        "description": "Description 1",
+                        "link": "https://filmstrip.com",
+                        "files": [
+                            {
+                                "filename": "a1498d35-5092-4c85-9895-a52aab24eddc.jpeg",
+                                "handle": "SU8Lk6RlQXqa7zya6NN6",
+                                "mimetype": "image/jpeg",
+                                "originalPath": "a1498d35-5092-4c85-9895-a52aab24eddc.jpeg",
+                                "size": 276444,
+                                "source": "local_file_system",
+                                "url": "https://cdn.filestackcontent.com/SU8Lk6RlQXqa7zya6NN6",
+                                "uploadId": "rbjaTqbGFRh7UpEY",
+                                "originalFile": {
+                                "name": "a1498d35-5092-4c85-9895-a52aab24eddc.jpeg",
+                                "type": "image/jpeg",
+                                "size": 276444
+                                },
+                                "status": "Stored"
+                            }
+                        ]
+                    },
+                    {
+                        "no": 2,
+                        "title": "Frame2",
+                        "description": "Description 2",
+                        "link": "https://filmstrip.com/2"
                     }
-                    ]
-                },
-                {
-                    "no": 2,
-                    "title": "Frame2",
-                    "description": "Description 2",
-                    "link": "https://filmstrip.com/2"
-                }
                 ],
                 "createdAt": "2019-08-10T01:03:47.073Z",
                 "createdBy": "system",
