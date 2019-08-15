64185491edf4178b48063c65c80abd810b4f4e14
softwarerero <el@softwarerero.com>
2019-08-15 12:54:20 -0700

    Add authentification

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Layout from './components/Layout'
 
 import { SignUp } from '/imports/ui/components/users/SignUp.jsx'
@@ -26,21 +26,27 @@ const AppRoute = ({ component: RouteComponent, layout: RouteLayout, ...rest }) =
     )} />
 )
 
+const PrivateRoute = ({ component: RouteComponent, layout: RouteLayout, ...rest }) =>
+    <Route {...rest} render={(props) => (
+        Meteor.userId()
+            ? <RouteLayout><RouteComponent {...props} /></RouteLayout> 
+            : <Redirect to='/signIn' />
+    )} />
+
 export default App = () =>
     <Router>
         <Switch>
             <AppRoute exact path="/" component={Home} layout={Layout} />
-            <AppRoute path="/filmstrips" component={FilmstripsList} layout={Layout} />
-            <AppRoute path="/filmstrip/:filmstripId/:frameId" component={FilmstripsItem} layout={Layout} />
-            <AppRoute exact path="/filmstrip" component={Filmstrip} layout={Layout} />
             <AppRoute exact path="/signUp" component={SignUp} layout={Layout} />
             <AppRoute exact path="/signIn" component={SignIn} layout={Layout} />
+            <PrivateRoute path="/filmstrips" component={FilmstripsList} layout={Layout} />
+            <PrivateRoute path="/filmstrip/:filmstripId/:frameId" component={FilmstripsItem} layout={Layout} />
+            <PrivateRoute exact path="/filmstrip/:filmstripId/:frameId/recordVideo" component={FrameVideoRecorder} layout={AnswerLayout} />
+            <PrivateRoute exact path="/recordVideo/:filmstripId/:frameId" component={FrameVideoRecorder} layout={AnswerLayout} />
             <AppRoute exact path="/a/:id" component={AnswerLanding} layout={AnswerLayout} />
             <AppRoute exact path="/a/:id/:emailBase64" component={AnswerLanding} layout={AnswerLayout} />
             <AppRoute exact path="/a/:id/:emailBase64/q" component={AnswerQuestionnaire} layout={AnswerLayout} />
             <AppRoute exact path="/a/:id/:emailBase64/finish" component={AnswerFinish} layout={AnswerLayout} />
-            <AppRoute exact path="/filmstrip/:filmstripId/:frameId/recordVideo" component={FrameVideoRecorder} layout={AnswerLayout} />
-            <AppRoute exact path="/recordVideo/:filmstripId/:frameId" component={FrameVideoRecorder} layout={AnswerLayout} />
             <AppRoute component={NoMatch} layout={Layout} />
         </Switch>
     </Router>
