a7fff5ad15f21fd1484263c4b65d4d8d5bd1c5af
Chuck Carpenter <chuck.carpenter@me.com>
2019-08-21 14:53:43 -0700

    chore: add code style enforcements across the app

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  useTabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+};
