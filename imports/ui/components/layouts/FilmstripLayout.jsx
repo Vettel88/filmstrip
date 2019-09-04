5363f856351bb2809f0ba6966937cd4ce475acfa
Dave Rogers <dave@incnition.com>
2019-09-04 11:51:00 -0700

    IQ-79 fix the responded count everywhere

--- imports/ui/components/layouts/FilmstripLayout.jsx
+++ imports/ui/components/layouts/FilmstripLayout.jsx
@@ -1,17 +1,30 @@
-import React, { Component } from 'react'
-import { Link, Route } from 'react-router-dom'
-import { MenuSurfaceAnchor, Menu, Grid, MenuItem, ListDivider, IconButton, Typography, GridCell, 
-    TopAppBar as UnstyledTopAppBar, TopAppBarSection, TopAppBarNavigationIcon, TopAppBarRow, TopAppBarTitle } from 'rmwc'
-import { SnackbarQueue } from '@rmwc/snackbar'
-import styled from 'styled-components'
-import { Meteor } from 'meteor/meteor'
-import { observer } from 'mobx-react'
-import { withRouter } from 'react-router'
-import { addTranslations, t, withTranslation } from '/imports/ui/UIHelpers.js'
-import { Notifications } from '/imports/ui/components/layouts/Notifications.jsx'
+import {
+    Grid,
+    GridCell,
+    IconButton,
+    ListDivider,
+    Menu,
+    MenuItem,
+    MenuSurfaceAnchor,
+    TopAppBarNavigationIcon,
+    TopAppBarRow,
+    TopAppBarSection,
+    TopAppBarTitle,
+    TopAppBar as UnstyledTopAppBar
+} from 'rmwc'
+import { addTranslations, t } from '/imports/ui/UIHelpers.js'
+import { Filmstrips } from '/imports/db/filmstrips.js'
 import { Invites } from '/imports/db/invites.js'
 import { invitesStore } from '/imports/store/invitesStore.js'
-import { Filmstrips } from '/imports/db/filmstrips.js'
+import { Meteor } from 'meteor/meteor'
+import { Notifications } from '/imports/ui/components/layouts/Notifications.jsx'
+import { observer } from 'mobx-react'
+import { Route } from 'react-router-dom'
+import { SnackbarQueue } from '@rmwc/snackbar'
+import { withRouter } from 'react-router'
+import { withTracker } from 'meteor/react-meteor-data'
+import React from 'react'
+import styled from 'styled-components'
 import stores from '/imports/store'
 import './FilmstripLayout.less'
 const { filmstripStore } = stores
@@ -27,7 +40,8 @@ const TopAppBar = styled(UnstyledTopAppBar)`
     background-color: #25455b !important;
 `
 
-const goto = (history, filmstripId, frameId, page) => () => history.push(`/filmstrip/${filmstripId}/${frameId}/${page}`)
+const goto = (history, filmstripId, frameId, page) => () =>
+    history.push(`/filmstrip/${filmstripId}/${frameId}/${page}`)
 const logout = history => () => {
     Meteor.logout()
     history.push('/signIn')
@@ -37,68 +51,129 @@ const toggleOpen = (open, setOpen) => () => setOpen(!open)
 const FilmstripMenu = () => {
     const [open, setOpen] = React.useState(false)
     return (
-        <Route render={({ history, match }) => {
-            const { filmstripId, frameId } = match.params
+        <Route
+            render={({ history, match }) => {
+                const { filmstripId, frameId } = match.params
 
-            Meteor.subscribe('Invites', () => {
-                invitesStore.invitesCount = Invites.find({ filmstripId }).count()
-            })
-            Meteor.subscribe('Filmstrips', () => {
-                invitesStore.respondedCount = Filmstrips.find({ responseToFilmstripId: filmstripId }).count()
-            })
-        
-            return (<MenuSurfaceAnchor>
-                <Menu open={open} onClose={() => setOpen(false)} anchorCorner='bottomStart'>
-                    <MenuItem onClick={goto(history, filmstripId, frameId, 'settings')}>{t('FilmstripLayout.Settings')}</MenuItem>
-                    <MenuItem onClick={goto(history, filmstripId, frameId, 'frames')}>{t('FilmstripLayout.Frames')}</MenuItem>
-                    <MenuItem onClick={goto(history, filmstripId, frameId, 'invites')}>{t('FilmstripLayout.Invites')} ({invitesStore.invitesCount})</MenuItem>
-                    <MenuItem onClick={goto(history, filmstripId, frameId, 'responded')}>{t('FilmstripLayout.Responded')} ({invitesStore.respondedCount})</MenuItem>
-                    <ListDivider />
-                    <MenuItem onClick={logout(history)}>Logout</MenuItem>
-                </Menu>
-                <IconButton icon="more_vert" onClick={toggleOpen(open, setOpen)} />
-            </MenuSurfaceAnchor>)
-        }}/>
+                return (
+                    <MenuSurfaceAnchor>
+                        <Menu
+                            open={open}
+                            onClose={() => setOpen(false)}
+                            anchorCorner='bottomStart'>
+                            <MenuItem
+                                onClick={goto(
+                                    history,
+                                    filmstripId,
+                                    frameId,
+                                    'settings'
+                                )}>
+                                {t('FilmstripLayout.Settings')}
+                            </MenuItem>
+                            <MenuItem
+                                onClick={goto(
+                                    history,
+                                    filmstripId,
+                                    frameId,
+                                    'frames'
+                                )}>
+                                {t('FilmstripLayout.Frames')}
+                            </MenuItem>
+                            <MenuItem
+                                onClick={goto(
+                                    history,
+                                    filmstripId,
+                                    frameId,
+                                    'invites'
+                                )}>
+                                {t('FilmstripLayout.Invites')} (
+                                {invitesStore.invitesCount})
+                            </MenuItem>
+                            <MenuItem
+                                onClick={goto(
+                                    history,
+                                    filmstripId,
+                                    frameId,
+                                    'responded'
+                                )}>
+                                {t('FilmstripLayout.Responded')} (
+                                {invitesStore.respondedCount})
+                            </MenuItem>
+                            <ListDivider />
+                            <MenuItem onClick={logout(history)}>
+                                Logout
+                            </MenuItem>
+                        </Menu>
+                        <IconButton
+                            icon='more_vert'
+                            onClick={toggleOpen(open, setOpen)}
+                        />
+                    </MenuSurfaceAnchor>
+                )
+            }}
+        />
     )
 }
 
-const Header = withRouter(observer(({ history }) => <TopAppBar fixed={false}>
-    <TopAppBarRow>
-        <TopAppBarSection>
-            <TopAppBarNavigationIcon icon="keyboard_arrow_left" onClick={() => history.push('/')} />
-        </TopAppBarSection>
-        <TopAppBarSection>
-            <TopAppBarTitle>
-                {filmstripStore.filmstrip && filmstripStore.filmstrip.name}
-            </TopAppBarTitle>
-        </TopAppBarSection>
-        <TopAppBarSection alignEnd>
-            <FilmstripMenu/>
-        </TopAppBarSection>
-    </TopAppBarRow>
-</TopAppBar>))
+const Header = withRouter(
+    observer(({ history }) => (
+        <TopAppBar fixed={false}>
+            <TopAppBarRow>
+                <TopAppBarSection>
+                    <TopAppBarNavigationIcon
+                        icon='keyboard_arrow_left'
+                        onClick={() => history.push('/')}
+                    />
+                </TopAppBarSection>
+                <TopAppBarSection>
+                    <TopAppBarTitle>
+                        {filmstripStore.filmstrip &&
+                            filmstripStore.filmstrip.name}
+                    </TopAppBarTitle>
+                </TopAppBarSection>
+                <TopAppBarSection alignEnd>
+                    <FilmstripMenu />
+                </TopAppBarSection>
+            </TopAppBarRow>
+        </TopAppBar>
+    ))
+)
 
-export default class FilmstripLayout extends Component {
-    render() {
-        return (<div className="FilmstripLayout">
+const FilmstripLayout = props => {
+    return (
+        <div className='FilmstripLayout'>
             <Header />
             <SnackbarQueue messages={Notifications.messages} timeout={10000} />
             <StyledGrid>
-                <GridCell span={12}>
-                    {this.props.children}
-                </GridCell>
+                <GridCell span={12}>{props.children}</GridCell>
             </StyledGrid>
-        </div>)
-    }
+        </div>
+    )
 }
 
+export default withRouter(
+    withTracker(({ match }) => {
+        const { filmstripId, frameId } = match.params
+
+        Meteor.subscribe('Invites', () => {
+            invitesStore.invitesCount = Invites.find({ filmstripId }).count()
+        })
+        Meteor.subscribe('Filmstrips', () => {
+            invitesStore.respondedCount = Filmstrips.find({
+                responseToFilmstripId: filmstripId
+            }).count()
+        })
+        return { filmstripId, frameId }
+    })(FilmstripLayout)
+)
+
 Meteor.startup(() => {
-    addTranslations('en', {    
+    addTranslations('en', {
         FilmstripLayout: {
             Settings: 'Settings',
             Frames: 'Frames',
             Invites: 'Invites',
-            Responded: 'Responded',
+            Responded: 'Responded'
         }
     })
     addTranslations('es', {
@@ -106,7 +181,7 @@ Meteor.startup(() => {
             Settings: 'Ajustes',
             Frames: 'Frames',
             Invites: 'Invitados',
-            Responded: 'Respondido',
+            Responded: 'Respondido'
         }
     })
 })
