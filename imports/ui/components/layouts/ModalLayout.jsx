6ce3cee8a9da723bf9bd23324fcca153a09dc086
Dave Rogers <dave@incnition.com>
2019-09-04 19:25:48 -0700

    hiding the modal for iOS webkit browsers that show an action menu

--- imports/ui/components/layouts/ModalLayout.jsx
+++ imports/ui/components/layouts/ModalLayout.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
 import { Dialog, DialogContent } from 'rmwc'
+import { isIOS } from 'react-device-detect'
 import { withRouter } from 'react-router'
+import React from 'react'
 
-const ModalLayout = withRouter((props) => {
-    const [open, setOpen] = React.useState(true);
-    return (
+const ModalLayout = withRouter(props => {
+    const [open, setOpen] = React.useState(true)
+    return isIOS ? (
+        <div className='hiddenModal'>{props.children}</div>
+    ) : (
         <Dialog
             open={open}
             onClose={_ => {
                 props.history.goBack()
-                setOpen(false)}
-            }
-        >
+                setOpen(false)
+            }}>
             <DialogContent>
-                { props.responsive ?
-                    <div className="setModalWidth">{props.children}</div> :
+                {props.responsive ? (
+                    <div className='setModalWidth'>{props.children}</div>
+                ) : (
                     props.children
-                }
+                )}
             </DialogContent>
         </Dialog>
-    );
+    )
 })
 
 export default ModalLayout
