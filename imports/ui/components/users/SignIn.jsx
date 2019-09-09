750f7546fb5e9b0bc390b3d10f2d52a242ec1dae
Juho Hietala <hietala@gmail.com>
2019-09-09 20:38:39 +0200

    Use outlined inputs in signup and signin

--- imports/ui/components/users/SignIn.jsx
+++ imports/ui/components/users/SignIn.jsx
@@ -2,104 +2,105 @@ import { Meteor } from 'meteor/meteor'
 import React, { useState } from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import { Icon, TextField, Typography } from 'rmwc'
-import {
-  validatePassword,
-  loadingWrapper
-} from '/imports/ui/UIHelpers.js'
+import { validatePassword, loadingWrapper } from '/imports/ui/UIHelpers.js'
 import { withTranslation } from 'react-i18next'
 import { PaddedCard as Card } from '/imports/ui/components/Cards.jsx'
-import { Form, BigButton as Button, ErrorNotice } from '/imports/ui/components/Forms.jsx'
+import {
+    Form,
+    BigButton as Button,
+    ErrorNotice
+} from '/imports/ui/components/Forms.jsx'
 
 const SignInForm = ({ t }) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isPasswordInvalid, setIsPasswordInvalid] = useState(false)
+    const [hasError, setHasError] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    const [toHome, redirectToHome] = useState(false)
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [isPasswordInvalid, setIsPasswordInvalid] = useState(false)
-  const [hasError, setHasError] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-  const [toHome, redirectToHome] = useState(false)
+    const handlePassword = event => {
+        setPassword(event.target.value)
+        const passwordValid = validatePassword(event.target.value)
+        setIsPasswordInvalid(!passwordValid)
+    }
 
-  const handlePassword = event => {
-    setPassword(event.target.value)
-    const passwordValid = validatePassword(event.target.value)
-    setIsPasswordInvalid(!passwordValid)
-  }
+    const handleSubmit = event => {
+        event.preventDefault()
 
-  const handleSubmit = event => {
-    event.preventDefault()
+        setIsLoading(true)
 
-    setIsLoading(true)
+        Meteor.loginWithPassword(email, password, (err, res) => {
+            setIsLoading(false)
 
-    Meteor.loginWithPassword(email, password, (err, res) => {
+            if (err) {
+                console.error(err)
+                setHasError(true)
+            } else {
+                redirectToHome(true)
+            }
+        })
+    }
 
-      setIsLoading(false)
+    if (toHome) {
+        return <Redirect push to='/' />
+    }
 
-      if(err) {
-        console.error(err)
-        setHasError(true)
-      }
-      else {
-        redirectToHome(true)
-      }
-    })
-  }
-
-  if (toHome) {
-    return <Redirect push to='/' />
-  }
-
-  return (
-    <>
-      <Form fullWidth onSubmit={handleSubmit}>
-        {hasError ? <>
-          <ErrorNotice
-            text={t('SignIn.Error')}
-          />
-        </> : ''}
-        <TextField
-          name='email'
-          type='email'
-          onChange={(event) => setEmail(event.target.value.trim().toLowerCase())}
-          label={t('Signup.Email')}
-        />
-        <TextField
-          name='password'
-          type='password'
-          onChange={handlePassword}
-          label={t('Signup.Password')}
-          invalid={isPasswordInvalid}
-        />
-        { loadingWrapper(isLoading, () => <Button
-          label={t('SignIn.Button')}
-          disabled={
-              email &&
-              password &&
-              !isPasswordInvalid
-              ? false
-              : true
-          }
-          raised
-          /> )
-        }
-      </Form>
-    </>
-  )
+    return (
+        <>
+            <Form fullWidth onSubmit={handleSubmit}>
+                {hasError ? (
+                    <>
+                        <ErrorNotice text={t('SignIn.Error')} />
+                    </>
+                ) : (
+                    ''
+                )}
+                <TextField
+                    outlined
+                    name='email'
+                    type='email'
+                    onChange={event =>
+                        setEmail(event.target.value.trim().toLowerCase())
+                    }
+                    label={t('Signup.Email')}
+                />
+                <TextField
+                    outlined
+                    name='password'
+                    type='password'
+                    onChange={handlePassword}
+                    label={t('Signup.Password')}
+                    invalid={isPasswordInvalid}
+                />
+                {loadingWrapper(isLoading, () => (
+                    <Button
+                        label={t('SignIn.Button')}
+                        disabled={
+                            email && password && !isPasswordInvalid
+                                ? false
+                                : true
+                        }
+                        raised
+                    />
+                ))}
+            </Form>
+        </>
+    )
 }
 
 export const SignIn = withTranslation()(({ t }) => {
-  return (
-    <>
-      <Card>
-        <Typography use='headline4' tag='h4'>
-          {t('SignIn.Header')}
-        </Typography>
-        <SignInForm t={t} />
-        <Typography tag='p' use='body2'>
-          <Link to='/signUp'>
-            {t('SignIn.SignupLink')}
-          </Link>
-        </Typography>
-      </Card>
-    </>
-  )
+    return (
+        <>
+            <Card>
+                <Typography use='headline4' tag='h4'>
+                    {t('SignIn.Header')}
+                </Typography>
+                <SignInForm t={t} />
+                <Typography tag='p' use='body2'>
+                    <Link to='/signUp'>{t('SignIn.SignupLink')}</Link>
+                </Typography>
+            </Card>
+        </>
+    )
 })
